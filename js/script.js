763a1e5ac1970351bbfb23e95d466a01e09e17c7
Dima <dimfreelancerdn@gmail.com>
2021-01-04 00:41:35 +0300

    dop lesson добавил разные примеры заполнения таблицы

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** TODO собираем массив объектов  */
+const appData = [];
+console.log('appData: ', appData);
+
+appData.push( 
+    {   
+        'id':0,
+        'brand':'Mazda',
+        'model':'x10',
+        'year':2020,
+        'mileage':10000
+    } );
+    
+console.log('appData: ', appData);
+
+let id = 1;
+let brand = 'Opel';
+let model = 'CC';
+let year = 2019;
+let mileage = '50000';
+
+appData.push( {id, brand, model, year, mileage} );
+
+console.log('appData: ', appData); 
+
+/** напишем функцию добавки одной записи в массиив обктов */
+const addModelToAppData = (id, brand, model, year, mileage) => {
+    appData.push( {id, brand, model, year, mileage} );
+};
+
+
+addModelToAppData( 2, 'mazda', '3', 2010, 55000);
+addModelToAppData( 3, 'mazda', 'x3', 2010, 155000);
+addModelToAppData( 4, 'VW', 'x2', 2020, 45000);
+addModelToAppData( 5, 'VW', 'cc', 2005, 95000);
+
+
+console.log('appData: ', appData); 
